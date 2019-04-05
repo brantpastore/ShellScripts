a5a666f29069fa0165767d9f81f2c0a5fd6ea053
Brant Pastore <brant.pastore@gmail.com>
2019-04-04 20:10:35 -0700

    Create Shortcut Drive Switch.ps1
    Added Powershell script that changes the drive letter of shortcuts in a directory. (Not complete/working)

---
+++ PowerShell/Shortcut Drive Switch.ps1
@@ -0,0 +1,40 @@
+﻿## INCOMPLETE/NOT WORKING
+param (
+    [Parameter(Mandatory=$true)][string]$sourcePath,
+    [Parameter(Mandatory=$true)][string]$oldDriveLetter,
+    [Parameter(Mandatory=$true)][string]$newDriveLetter
+)
+
+function GetShortcuts {
+    $Shortcuts = Get-ChildItem -Recurse $sourcePath -Include *.lnk
+    $Shell = New-Object -ComObject WScript.Shell
+
+    foreach ($Shortcut in $Shortcuts) { ## Grab the target of the old shortcut
+        $Properties = @{
+            $Target = $Shell.CreateShortcut($Shortcut.FullName).TargetPath
+        }
+        $Target
+	    New-Object PSObject -Property $Properties         
+    }
+
+    [Runtime.InteropServices.Marshal]::ReleaseComObject($Shell) | Out-Null
+}
+
+
+function SetShortcuts {
+    $res = GetShortcuts
+    $count = 0
+
+    $shell = New-Object -ComObject WScript.Shell
+
+    foreach($Target in $res) {
+       if ($res.Target[$count].Substring(0, 2) -eq $oldDriveLetter) { 
+       echo $res.Target[$count].ink
+            $shortcut = $shell.CreateShortcut($res.Target[$count].ink)
+            $shortcut.Save()
+       }
+       $count++
+    }
+}
+
+SetShortcuts
