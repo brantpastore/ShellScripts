92453fa4703381b30c9e69d400e9fca3a70ee011
Brant Pastore <brant.pastore@gmail.com>
2021-05-04 20:39:21 -0500

    Add files via upload
    Added a Script to parse zerotier-cli info that returns our Node ID.
    Added an Example script to set <Node> on <Network> Description value.

---
+++ ZeroTier/GetZTNodeID.ps1
@@ -0,0 +1,10 @@
+﻿# We can use this to get only the Node ID as a string.
+
+$ztInfo = zerotier-cli info
+$ztInfo = $ztInfo.Replace('200 ','');
+$ztInfo = $ztInfo.Replace('info ','');
+$ztInfo = $ztInfo.Replace(' 1.6.2 ','');
+$ztInfo = $ztInfo.Replace('ONLINE','');
+
+$NodeID = $ztInfo
+Write-Host $NodeID;
---
+++ ZeroTier/SetZT_KeyValue_Member_Description_Example.ps1
@@ -0,0 +1,27 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$token = TOKENHERE
+
+$Description = "Test Success"
+
+$BaseUrl = "https://my.zerotier.com/api/network/"
+$NetworkURL = "<NETWORK ID HERE>/member/"
+$MemberURL = "<NODE ID HERE>"
+
+$FinalURL = $BaseUrl + $NetworkURL + $MemberURL
+
+$Body = @"
+{
+    "description": "$Description"
+}
+"@
+
+
+$Headers = @{
+    Authorization="Bearer $token"
+}
+
+$responseData = (Invoke-WebRequest -Uri $FinalURL -Method POST -Headers $Headers -UseBasicParsing -Body $Body -ContentType "application/json").Content | ConvertTo-Json
+
+
+Write-Host $responseData
